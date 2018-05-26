1377bbc402bd4a73b1013ebe5bee52efba612021
David Perez <david.perez@us.abb.com>
2018-05-26 10:33:30 -0400

    Add connection to wallet_db #18

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'wallet_db'
+    });
+};
+
+connection.connect();
+module.exports = connection;
