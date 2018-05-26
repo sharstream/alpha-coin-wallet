39f7f77c253c292cb555a195ef4b9ec1e4465f3a
David Perez <david.perez@us.abb.com>
2018-05-26 13:35:53 -0400

    Build mocha test #22

--- test/test.js
+++ test/test.js
@@ -2,14 +2,15 @@
 
 require('mocha');
 var assert = require('assert');
-var alphaCoinWallet = require('./');
+var alphaCoinWallet = require('../routes/api-accounts-routes');
+var alphaCoinInvoice = require('../routes/api-transactions-routes');
 
-describe('alpha-coin-wallet', function() {
+describe('Accounts', function() {
   it('should export a function', function() {
     assert.equal(typeof alphaCoinWallet, 'function');
   });
 
-  it('should export an object', function() {
+  it('should export an account', function() {
     assert(alphaCoinWallet);
     assert.equal(typeof alphaCoinWallet, 'object');
   });
@@ -17,6 +18,24 @@ describe('alpha-coin-wallet', function() {
   it('should throw an error when invalid args are passed', function() {
     assert.throws(function() {
       alphaCoinWallet();
+    });
+  });
+
+});
+
+describe('Transactions', function () {
+  it('should export a function', function () {
+    assert.equal(typeof alphaCoinInvoice, 'function');
+  });
+
+  it('should export an invoice', function () {
+    assert(alphaCoinInvoice);
+    assert.equal(typeof alphaCoinInvoice, 'object');
+  });
+
+  it('should throw an error when invalid args are passed', function () {
+    assert.throws(function () {
+      alphaCoinInvoice();
     });
   });
 
